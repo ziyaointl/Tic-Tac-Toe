5748402ea9cfe5ee49aa8116f0a9ed4bdb25fab2
Ziyao Zhang <ziyaointl@gmail.com>
2017-03-27 11:42:46 +0800

    Move Bulma to node_modules

--- src/App.vue
+++ src/App.vue
@@ -34,11 +34,5 @@
 </script>
 
 <style lang="scss">
-  @import "css/bulma/utilities/_all.sass";
-  @import "css/bulma/base/_all.sass";
-  @import "css/bulma/layout/_all.sass";
-  @import "css/bulma/elements/_all.sass";
-  @import "css/bulma/grid/_all.sass";
-  @import "css/bulma/components/_all.sass";
   @import "css/styles.scss";
 </style>
