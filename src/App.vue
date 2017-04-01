14ade89f87ae5da4c9ad20e94b130df5b1df2d0e
Ziyao Zhang <ziyaointl@gmail.com>
2017-04-02 03:03:23 +0800

    Implement fill mechanism

--- src/App.vue
+++ src/App.vue
@@ -18,19 +18,19 @@
           <div class="board">
             <table>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td @click="padClicked(0)"></td>
+                <td @click="padClicked(1)"></td>
+                <td @click="padClicked(2)"></td>
               </tr>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td @click="padClicked(3)"></td>
+                <td @click="padClicked(4)"></td>
+                <td @click="padClicked(5)"></td>
               </tr>
               <tr>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td @click="padClicked(6)"></td>
+                <td @click="padClicked(7)"></td>
+                <td @click="padClicked(8)"></td>
               </tr>
             </table>
           </div>
@@ -45,7 +45,8 @@
     name: 'app',
     data() {
       return {
-        isCircle: true
+        isCircle: true,
+        pads: document.getElementsByTagName("td")
       }
     },
     methods: {
@@ -58,16 +59,29 @@
       selectX() {
         this.closeModals();
         this.isCircle = false;
-        let elements = document.getElementsByTagName("td");
-        for (let i = 0; i < elements.length; ++i) {
-          elements[i].innerHTML = "<p>" + i + "</p>";
-        }
+        // let elements = document.getElementsByTagName("td");
+        // for (let i = 0; i < elements.length; ++i) {
+        //   elements[i].innerHTML = "<p>" + i + "</p>";
+        // }
       },
       selectO() {
         this.closeModals();
       },
       reset() {
         isCircle = true;
+      },
+      padClicked(i) {
+        if (this.pads[i].innerHTML === "") {
+          if (this.isCircle) {
+            this.pads[i].innerHTML = "<h2>O</p2>";
+          } else {
+            this.pads[i].innerHTML = "<h2>X</p2>";
+          }
+          this.isCircle = !this.isCircle;
+        }
+      },
+      checkWin() {
+        
       }
     }
   }
